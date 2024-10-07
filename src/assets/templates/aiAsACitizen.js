e5a05fb5fade548888797311c2ad616b0a023f95
Jordi Tost <tost.jordi@gmail.com>
2024-10-07 21:35:46 +0200

    Refine templates.

--- src/assets/templates/aiAsACitizen.js
+++ src/assets/templates/aiAsACitizen.js
@@ -6,12 +6,12 @@ export default {
   id: 'ai-as-a-citizen',
   prompts,
   name: {
-    en: 'AI as a Citizen 🤖🌆',
-    de: 'KI als Bürger*in 🤖🌆'
+    en: 'New Smart City Story 🤖🌆',
+    de: 'Neue Smart City Story 🤖🌆'
   },
   description: {
-    en: 'In which Smart City futures would you like to live?',
-    de: 'In welchen Smart-City-Zukünften würdest du gerne leben?'
+    en: 'AI as a Citizen: In which Smart City futures would you like to live?',
+    de: 'KI als Bürger*in: In welchen Smart-City-Zukünften würdest du gerne leben?'
   },
   template: {
     en: "<br><code>/////////////////////////////////<br><br>Let's setup the story first:<br><br>🧑🏽‍🎤 The protagonist<br>🦄 The others<br>🌐 The place where they live<br>🌟 The topic<br><br>/////////////////////////////////</code><br><br><br><start>→ Let AI start! 🔮✍️</start>",
