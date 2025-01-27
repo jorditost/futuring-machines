62b4fc44f7f0453ca4e3c5e4ff546ac65e948c6e
Jordi Tost <tost.jordi@gmail.com>
2025-01-27 17:54:27 +0100

    AI as a Citizen Template

--- src/assets/templates/index.js
+++ src/assets/templates/index.js
@@ -1,12 +1,12 @@
 //import blank from './blank.js'
 import setupStoryExample from './setupStoryExample.js'
-import herzberg from './herzberg.js'
-//import aiAsACitizen from './aiAsACitizen.js'
+//import herzberg from './herzberg.js'
+import aiAsACitizen from './aiAsACitizen.js'
 //import futuresOfDesign from './futuresOfDesign.js'
 //import thingFromTheFuture from './thingFromTheFuture.js'
 //import localCommunity from './localCommunityStart.js'
 //import nightOfWritingStem from './nightOfWritingStem.js'
 //import queerFuturesStem from './queerFuturesStem.js'
 
-export default [setupStoryExample, herzberg]
+export default [setupStoryExample, aiAsACitizen]
 //export default [blank, thingFromTheFuture, localCommunity, nightOfWritingStem, queerFuturesStem, futuresOfDesign]
